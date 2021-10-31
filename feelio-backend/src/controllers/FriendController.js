407cfff9e929e1bdf8c725f6b37287bea32450c0
FadingShadow <sfurqan1999@gmail.com>
2021-10-31 19:14:23 +0500

    Friend controller functions added and routes changed

--- feelio-backend/src/controllers/FriendController.js
+++ feelio-backend/src/controllers/FriendController.js
@@ -2,30 +2,81 @@ const { FriendsModel } = require("../models/FriendsModel");
 const { successResponse } = require("../utils/responses");
 
 class FriendController {
-    get(req, res, next){
-        let user_id = req.user_id;
-        function callback(errors, results){
-            if(errors){
+  get(req, res, next) {
+    const { user_id } = req;
+    function callback(errors, results) {
+      if (errors) {
+      } else {
+        res.json(
+          successResponse(
+            { ...results },
+            "Friend requests fetched successfully!"
+          )
+        );
+      }
+    }
+    FriendsModel.findPendingByUser({ user_id, cb: callback });
+  }
 
-            }
-            else{
-                res.json(successResponse({...results}, "Friend requests fetched successfully!"));
-            }
-        }
-        FriendsModel.findPendingByUser({user_id, cb: callback})
+  create(req, res, next) {
+    const { user_id } = req;
+    const { friend_id } = req.body;
+
+    function callback(errors, results) {
+      if (errors) {
+      } else {
+        res.json(successResponse(null, "Friend requests sent successfully!"));
+      }
     }
 
-    create(req, res, next){
+    FriendsModel.create({ user_id, friend_id, cb: callback });
+  }
 
+  accept(req, res, next) {
+    const { friend_request_id } = req.body;
+
+    function callback(errors, results) {
+      if (errors) {
+      } else {
+        res.json(
+          successResponse(null, "Friend request accepted successfully!")
+        );
+      }
     }
 
-    edit(req, res, next){
-        
+    FriendsModel.acceptRequest({ friend_request_id, cb: callback });
+  }
+
+  decline(req, res, next) {
+    const { friend_request_id } = req;
+
+    function callback(errors, results) {
+      if (errors) {
+      } else {
+        res.json(
+          successResponse(null, "Friend request declined successfully!")
+        );
+      }
     }
 
-    delete(req, res, next){
+    FriendsModel.declineRequest({ friend_request_id, cb: callback });
+  }
 
+  getFriends(req, res, next) {
+    const { user_id } = req;
+    function callback(errors, results) {
+      if (errors) {
+      } else {
+        res.json(
+          successResponse(
+            { ...results },
+            "Friend request declined successfully!"
+          )
+        );
+      }
     }
+    FriendsModel.findAcceptedByUser({ user_id, cb: callback });
+  }
 }
 
 module.exports.FriendController = new FriendController();
--- feelio-backend/src/routes/friendRequestRoutes.js
+++ feelio-backend/src/routes/friendRequestRoutes.js
@@ -3,13 +3,19 @@ const auth = require("../middlewares/AuthMiddleware");
 const { FriendController } = require("../controllers/FriendController");
 var friendsRouter = express.Router();
 
+/* Get Friend Requests */
+friendsRouter.get("/requests", auth, FriendController.get);
+
+/* Send Friend Request */
+friendsRouter.post("/requests", auth, FriendController.create);
+
+/* Accept Friend Request */
+friendsRouter.patch("/requests/accept", auth, FriendController.accept);
+
+/* Decline Friend Request */
+friendsRouter.patch("/requests/reject", auth, FriendController.decline);
+
 /* Get Friends */
-friendsRouter.get("/", auth, FriendController.get);
-
-/* Add Friend */
-friendsRouter.post("/", auth, FriendController.create);
-
-/* Remove Friend */
-friendsRouter.delete("/", auth, FriendController.delete);
+friendsRouter.get("/", auth, FriendController.getFriends);
 
 module.exports = friendsRouter;
