fae972755872492189392b2157076ce19fb313cc
diablo-shotgun <58742571+diablo-shotgun@users.noreply.github.com>
2021-10-31 20:08:03 +0500

    controllers

--- feelio-backend/src/controllers/PostController.js
+++ feelio-backend/src/controllers/PostController.js
@@ -1,24 +1,132 @@
 const { PostModel } = require("../models/PostModel");
+const { successResponse } = require("../utils/responses");
 
 class PostController{
 
-    get(req, res, next){
-        
+    getPost(req, res, next){
+        const { post_id } = req.body.postId;
+
         function callback(error, results){
-            
+            if (error) {
+                res.json({
+                    header: {
+                      error: "1",
+                      message: "Request Failed!",
+                    },
+                  });
+            } else {
+              res.json(
+                successResponse(
+                  { ...results },
+                  "Post fetched successfully!"
+                )
+              );
+            }
         }
+
+        PostModel.findById({ post_id, cb: callback });
+    }
+
+    getPostByFriends(req, res, next){
+        const { user_id } = req;
+
+        function callback(error, results){
+            if (error) {
+                res.json({
+                    header: {
+                      error: "1",
+                      message: "Request Failed!",
+                    },
+                  });
+            } else {
+              res.json(
+                successResponse(
+                  { ...results },
+                  "Posts fetched successfully!"
+                )
+              );
+            }
+        }
+
+        PostModel.findByFriends({ user_id, cb: callback });
     }
 
     create(req, res, next){
+        const { user_id } = req;
+        const { body } = req.body.body;
+        const { image } = req.body.image;
 
+        function callback(error, results){
+            if (error) {
+                res.json({
+                    header: {
+                      error: "1",
+                      message: "Failed to create Post!",
+                    },
+                  });
+            } else {
+              res.json(
+                successResponse(
+                  { ...results },
+                  "Posts created successfully!"
+                )
+              );
+            }
+        }
+
+        PostModel.create({body, image, user_id, cb: callback });
     }
 
     edit(req, res, next){
-        
+        const { user_id } = req;
+        const { body } = req.body.body;
+        const { image } = req.body.image;
+        const { post_id } = req.body.postId;
+
+        function callback(error, results){
+            if (error) {
+                res.json({
+                    header: {
+                      error: "1",
+                      message: "Failed to edit Post!",
+                    },
+                  });
+            } else {
+              res.json(
+                successResponse(
+                  { ...results },
+                  "Posts edited successfully!"
+                )
+              );
+            }
+        }
+
+        PostModel.edit({user_id, body, image, post_id, cb: callback });
     }
 
     delete(req, res, next){
+        const { user_id } = req;
+        const { post_id } = req.body.postId;
 
+        function callback(error, results){
+            if (error) {
+                res.json({
+                    header: {
+                      error: "1",
+                      message: "Failed to delete Post!",
+                    },
+                  });
+            } else {
+              res.json(
+                successResponse(
+                  { ...results },
+                  "Posts deleted successfully!"
+                )
+              );
+            }
+        }
+
+        PostModel.delete({user_id, post_id, cb: callback });
     }
 }
 
--- feelio-backend/src/models/PostModel.js
+++ feelio-backend/src/models/PostModel.js
@@ -11,10 +11,22 @@ class PostModel {
     );
   }
 
+  /*
   findByUser({ user_id, cb }) {
     DBService.dbPool.query(
       "SELECT body, image, user_id, posted_at from posts where user_id = ? ORDER BY post_id DESC",
       [user_id],
+      (error, results) => {
+        cb(error, results);
+      }
+    );
+  }
+  */
+
+  findByFriends({ user_id, cb }) {
+    DBService.dbPool.query(
+      "SELECT body, image, user_id, posted_at from posts where user_id IN (SELECT IF(sender_id = ?, receiver_id, sender_id) as user_id FROM friend_requests WHERE status = 'Accepted' AND (sender_id = ? OR receiver_id = ?)) ORDER BY post_id DESC",
+      [user_id, user_id, user_id],
       (error, results) => {
         cb(error, results);
       }
@@ -31,10 +43,10 @@ class PostModel {
     );
   }
 
-  edit({ body, image, post_id, cb }) {
+  edit({ user_id, body, image, post_id, cb }) {
     DBService.dbPool.query(
-      "UPDATE posts SET body = ?, image = ? WHERE post_id = ?",
-      [body, image, post_id],
+      "UPDATE posts SET body = ?, image = ? WHERE post_id = ? AND user_id = ?",
+      [body, image, post_id, user_id],
       (error, results) => {
         cb(error, results.insertId);
       }
@@ -42,10 +54,10 @@ class PostModel {
   }
 
 
-  delete({ post_id, cb }) {
+  delete({ user_id, post_id, cb }) {
     DBService.dbPool.query(
-      "DELETE FROM posts WHERE post_id = ?",
-      [post_id],
+      "DELETE FROM posts WHERE post_id = ? AND user_id = ?",
+      [post_id, user_id],
       (error, results) => {
         cb(error, results.insertId);
       }
